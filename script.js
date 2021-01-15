9ad76201297147005419b6bcc4e0ad0d5bf2ad7e
banuakman <banu@asterya.com>
2021-01-14 21:14:59 -0500

    Added Pseudo code

---
+++ script.js
@@ -0,0 +1,16 @@
+// When button clicked 
+// Start a function to generate password
+
+// Ask (prompt) the length 
+    // Check a length of at least 8 characters and no more than 128 characters
+
+// Confirm the following 
+    // Special characters (see examples)
+    // Numeric characters
+    // Lowercase characters
+    // Uppercase characters
+
+// Validate user input and ensure that at least one character type is selected
+    // If 4 negative choices selected Alert to choose one
+
+//Function to create password when user input is validated
