ad5a2f21c8950d906664cd8b9de2fa3d003b4c44
banuakman <banu@asterya.com>
2021-01-14 21:56:36 -0500

    Added array values for variables

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
+// Variables ====================================
+// User Inputs
+var characterLength
+var confirmSpecial
+var confirmNumber
+var confirmUppercase
+var confirmLowercase
+
+// Password variable values
+    // Special characters
+special = ["!", "#", "$", "%", "&", "/", "(", ")", "=", "*", "?", "@", "+", ">", "<"];
+    // Numeric characters
+number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+    // Lowercase characters
+lowercaseLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+    // Uppercase characters
+var toUpper = function (letters) {
+        return letters.toUpperCase();
+    };
+uppercaseLetters = lowercaseLetters.map(toUpper);
+
+console.log (uppercaseLetters)
+
+//Functions=======================================
 // When button clicked 
 // Start a function to generate password
 
@@ -5,7 +29,7 @@
     // Check a length of at least 8 characters and no more than 128 characters
 
 // Confirm the following 
-    // Special characters (see examples)
+    // Special characters
     // Numeric characters
     // Lowercase characters
     // Uppercase characters
